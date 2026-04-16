28d735645822ee1493f2866352b15079fbae9dcb
nurill <mnurilhuda2005@gmail.com>
2026-04-16 15:27:14 +0700

    Membuat File Model

---
+++ app/Models/BarangModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class BarangModel extends Model
+{
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+
+    protected $fillable = [
+        'kategori_id',
+        'barang_kode',
+        'barang_nama',
+        'harga_beli',
+        'harga_jual',
+    ];
+
+    public function kategori(): BelongsTo
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    public function stok(): HasMany
+    {
+        return $this->hasMany(StokModel::class, 'barang_id', 'barang_id');
+    }
+
+    public function detailPenjualan(): HasMany
+    {
+        return $this->hasMany(PenjualanDetailModel::class, 'barang_id', 'barang_id');
+    }
+}
---
+++ app/Models/UserModel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class UserModel extends Model
+{
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function penjualan(): HasMany
+    {
+        return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
+    }
+
+    public function stok(): HasMany
+    {
+        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    }
+}
